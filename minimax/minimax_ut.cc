0b0a1dd65705a2947762432cace3a5a66fe50355
Evgeny Shulgin <izaronplatz@gmail.com>
2022-02-22 01:21:28 +0300

    Add simple tests for minimax

---
+++ minimax/minimax_ut.cc
@@ -0,0 +1,91 @@
+#include <gtest/gtest.h>
+#include "minimax.h"
+#include "testing.h"
+#include "knight_piece.h"
+#include "queen_piece.h"
+#include "rook_piece.h"
+#include "pawn_piece.h"
+
+using namespace NFairyChess;
+using namespace NFairyChess::NVanillaPieces;
+
+const auto WhiteQueen = TBoardPiece::Create<TQueenPiece>(EPieceColor::White);
+const auto BlackRook = TBoardPiece::Create<TRookPiece>(EPieceColor::Black);
+const auto BlackKnight = TBoardPiece::Create<TKnightPiece>(EPieceColor::Black);
+const auto BlackPawn = TBoardPiece::Create<TPawnPiece>(EPieceColor::Black);
+
+TEST(MinimaxTest, SimpleBestCapture) {
+    auto board1 = TBoard{}
+        .SetBoardPiece({.Column = 3, .Row = 3}, WhiteQueen)
+        .SetBoardPiece({.Column = 5, .Row = 3}, BlackRook)
+        .SetBoardPiece({.Column = 3, .Row = 5}, BlackKnight);
+
+    std::string_view dump1 =
+        "╔════════╗"
+        "║        ║"
+        "║        ║"
+        "║   ♞    ║"
+        "║        ║"
+        "║   ♕ ♜  ║"
+        "║        ║"
+        "║        ║"
+        "║        ║"
+        "╚════════╝";
+    CheckDump(dump1, board1);
+
+    TMinimax minimax{/* depth = */ 1};
+    TMove move = minimax.FindBestMove(board1, EPieceColor::White);
+
+    TBoard board2 = ApplyMove(board1, move);
+    std::string_view dump2 =
+        "╔════════╗"
+        "║        ║"
+        "║        ║"
+        "║   ♞    ║"
+        "║        ║"
+        "║     ♕  ║"
+        "║        ║"
+        "║        ║"
+        "║        ║"
+        "╚════════╝";
+    CheckDump(dump2, board2);
+}
+
+TEST(MinimaxTest, CaptureSearchProlongation) {
+    auto board1 = TBoard{}
+        .SetBoardPiece({.Column = 3, .Row = 3}, WhiteQueen)
+        .SetBoardPiece({.Column = 5, .Row = 3}, BlackRook)
+        .SetBoardPiece({.Column = 3, .Row = 5}, BlackKnight)
+        .SetBoardPiece({.Column = 6, .Row = 4}, BlackPawn);
+
+
+    std::string_view dump1 =
+        "╔════════╗"
+        "║        ║"
+        "║        ║"
+        "║   ♞    ║"
+        "║      ♟︎ ║"
+        "║   ♕ ♜  ║"
+        "║        ║"
+        "║        ║"
+        "║        ║"
+        "╚════════╝";
+    CheckDump(dump1, board1);
+
+    TMinimax minimax{/* depth = */ 1};
+    TMove move = minimax.FindBestMove(board1, EPieceColor::White);
+
+    TBoard board2 = ApplyMove(board1, move);
+    std::string_view dump2 =
+        "╔════════╗"
+        "║        ║"
+        "║        ║"
+        "║   ♕    ║"
+        "║      ♟︎ ║"
+        "║     ♜  ║"
+        "║        ║"
+        "║        ║"
+        "║        ║"
+        "╚════════╝";
+    CheckDump(dump2, board2);
+}
